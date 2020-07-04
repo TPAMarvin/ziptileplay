083908c8516edde15cf19dcb1589c5ff3825ea9f
TPAMarvin <31587596+TPAMarvin@users.noreply.github.com>
2020-07-04 15:55:33 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,58 @@
+let tileDisplay = Kitronik_Zip_Tile.createZIPTileDisplay(1, 1, Kitronik_Zip_Tile.UBitLocations.Visible)
+tileDisplay.setBrightness(50)
+basic.showNumber(1)
 basic.forever(function () {
-	
+    for (let drawing = 0; drawing <= 5; drawing++) {
+        tileDisplay.setMatrixColor(drawing, 0, Kitronik_Zip_Tile.colors(ZipLedColors.Red))
+    }
+    tileDisplay.rotate(8)
+    tileDisplay.show()
+    basic.pause(500)
+    for (let drawing = 0; drawing <= 5; drawing++) {
+        tileDisplay.setMatrixColor(drawing + 1, 0, Kitronik_Zip_Tile.colors(ZipLedColors.Red))
+    }
+    tileDisplay.setMatrixColor(6, 0, Kitronik_Zip_Tile.colors(ZipLedColors.Blue))
+    tileDisplay.setMatrixColor(5, 0, Kitronik_Zip_Tile.colors(ZipLedColors.White))
+    tileDisplay.rotate(8)
+    tileDisplay.show()
+    basic.pause(500)
+    for (let drawing = 0; drawing <= 7; drawing++) {
+        tileDisplay.setMatrixColor(drawing, 0, Kitronik_Zip_Tile.colors(ZipLedColors.Red))
+    }
+    tileDisplay.setMatrixColor(6, 0, Kitronik_Zip_Tile.colors(ZipLedColors.White))
+    tileDisplay.setMatrixColor(5, 0, Kitronik_Zip_Tile.colors(ZipLedColors.White))
+    tileDisplay.rotate(8)
+    tileDisplay.show()
+    basic.pause(500)
+    for (let drawing = 0; drawing <= 6; drawing++) {
+        tileDisplay.setMatrixColor(drawing + 1, 0, Kitronik_Zip_Tile.colors(ZipLedColors.Red))
+    }
+    tileDisplay.rotate(8)
+    tileDisplay.show()
+    basic.pause(500)
+    for (let drawing = 0; drawing <= 7; drawing++) {
+        tileDisplay.setMatrixColor(drawing, 0, Kitronik_Zip_Tile.colors(ZipLedColors.Red))
+    }
+    tileDisplay.setMatrixColor(6, 0, Kitronik_Zip_Tile.colors(ZipLedColors.Blue))
+    tileDisplay.setMatrixColor(5, 0, Kitronik_Zip_Tile.colors(ZipLedColors.White))
+    tileDisplay.rotate(8)
+    tileDisplay.show()
+    basic.pause(500)
+    for (let drawing = 0; drawing <= 5; drawing++) {
+        tileDisplay.setMatrixColor(drawing + 1, 0, Kitronik_Zip_Tile.colors(ZipLedColors.Red))
+    }
+    tileDisplay.setMatrixColor(6, 0, Kitronik_Zip_Tile.colors(ZipLedColors.White))
+    tileDisplay.setMatrixColor(5, 0, Kitronik_Zip_Tile.colors(ZipLedColors.White))
+    tileDisplay.rotate(8)
+    tileDisplay.show()
+    basic.pause(500)
+    for (let drawing = 0; drawing <= 5; drawing++) {
+        tileDisplay.setMatrixColor(drawing, 0, Kitronik_Zip_Tile.colors(ZipLedColors.Red))
+    }
+    tileDisplay.rotate(8)
+    tileDisplay.show()
+    basic.pause(500)
+    tileDisplay.rotate(8)
+    tileDisplay.show()
+    basic.pause(500)
 })
